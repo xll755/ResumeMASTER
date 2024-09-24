45f7694d3f7f4bcbd682a5cfb91f352ffab4b595
justice <81177526+tPeltier@users.noreply.github.com>
2024-09-24 03:05:44 -0400

    (user.php) protect id property
    can most likely be improved

--- www/User.php
+++ www/User.php
@@ -1,10 +1,11 @@
 <?php
 class User
 {
-	public int $id;
+	protected int $id; // or private?
 	public string $firstName;
 	public string $lastName;
 	public string $email;
 
+	public function setID(int $id):void { $this->id = $id; } // for testing & creation only rn  TODO: repair
 }
 ?>

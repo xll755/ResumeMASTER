59e8a94790a20b9d27a5e1a7dbb3d699df20a914
justice <81177526+tPeltier@users.noreply.github.com>
2024-09-24 21:25:14 -0400

    init implementation of Resume funcs
    essentially a copy of users.php

--- www/Resume.php
+++ www/Resume.php
@@ -1,8 +1,67 @@
 <?php
-class Resume
+class Resume implements DB_functions
 {
-	public int $id;
+	protected int $id; // or private?
 	public int $userId;
 	public string $name;
+
+	public function setID(int $id):void { $this->id = $id; } // for testing & creation only rn  TODO: repair
+
+	public function create(mysqli $mysqli): void
+	{
+		$query = "insert into resumes(userId, name) values (?,?)";
+		$stmt = $mysqli->prepare($query);
+		$userId = $this->userId;
+		$name = $this->name;
+		$types = "ss";
+		$stmt->bind_param($types, $userId, $name);
+		$stmt->execute();
+	}
+
+	public function delete(mysqli $mysqli): void
+	{
+		$query = "delete from resumes where resumes.id = (?)";
+		$stmt = $mysqli->prepare($query);
+		$id = this->id;
+		$types = "i";
+		$stmt->bind_param($types, $id);
+		$stmt->execute();
+	}
+
+	// NOTE: make static & return obj???
+	public function fetch(mysqli $mysqli, int $id): void
+	{
+		$query = "select * from resumes where resumes.id = $id"; // WARN: best practice???
+		$stmt = $mysqli->prepare($query);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		$row = $result->fetch_assoc();
+		$this->id = $row['id'];
+		$this->userId = $row['userId'];
+		$this->name = $row['name'];
+	}
+
+	public function insert(mysqli $mysqli, string $field): void
+	{
+		$query = "insert into resumes(field) values (?) where resumes.id = (?)";
+		$stmt = $mysqli->prepare($query);
+		$field = $field; // necessary?
+		$id = $this->id;
+		$types = "si";
+		$stmt->bind_param($types, $field, $id);
+		$stmt->execute();
+	}
+
+	public function update(mysqli $mysqli, string $field): void
+	{
+		$query = "update resmues(field) set (?) where resumes.id = (?)";
+		$stmt = $mysqli->prepare($query);
+		$field = $field; // necessary?
+		$id = $this->id;
+		$types = "si";
+		$stmt->bind_param($types, $field, $id);
+		$stmt->execute();
+	}
+
 }
 ?>

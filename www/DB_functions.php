a800ed700c3c2a71d95810e513a250d9f4c94126
justice <81177526+tPeltier@users.noreply.github.com>
2024-09-27 20:13:59 -0400

    update interface functions

--- www/DB_functions.php
+++ www/DB_functions.php
@@ -4,8 +4,7 @@ interface  DB_functions
 	// we could make these return bool for p/f
 	public function create(mysqli $mysqli): void;
 	public function delete(mysqli $mysqli): void;
-	public function fetch(mysqli $mysqli, int $id): void; // return obj?
-	public function insert(mysqli $mysqli, string $field): void;
-	public function update(mysqli $mysqli, string $field): void;
+	public function pull(mysqli $mysqli, int $id): void;
+	public function push(mysqli $mysqli): void;
 }
 ?>

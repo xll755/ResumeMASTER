3f8dac93dadc6e3b1e828d494e1feb9ea82d509a
justice <81177526+tPeltier@users.noreply.github.com>
2024-09-27 20:20:53 -0400

    update testing done in index.php

--- www/index.php
+++ www/index.php
@@ -4,30 +4,71 @@
 $mysqli = require_once "./db_config.php"; // assignment not strictly necessary but seems to help lsp, kinda
 include "./DB_functions.php";
 include "./User.php";
-include "./Resume.php";
+// include "./Resume.php";
 
+print("testing db connection: \n");
 $sql = "select database();";
 $result = $mysqli->query($sql);
 $result = $result->fetch_array();
 echo 'current db: ' . $result['database()'] . "\n";
 
+print("-------------- \n");
+/*
+* here's my vision as of right now:
+* we work with user objects "locally"
+* then update the db based on the state of the obj
+*/
+
 // create test user
 $test_user = new User();
-// $test_user->setID(1);
+// $test_user->setID(1); // set by db
+$test_user->userName = 'test_user';
 $test_user->firstName = 'test';
 $test_user->lastName = 'user';
 $test_user->email = 'test_user@test.test';
-
+$test_user->setPW('passwdHash');
+print("show created user obj: \n");
 print_r($test_user); // "cleaner"
 var_dump($test_user); // more precise
 
-echo "testing create function: \n";
-
+print("-------------- \n");
+echo "testing user.create method: \n";
+$sql = "select count(*) from users;";
+$result = $mysqli->query($sql);
+$result = $result->fetch_array();
+echo 'current # of users: ' . $result['count(*)'] . "\n";
+print("creating db entry for user... \n");
 $test_user->create($mysqli);
-$user_from_db = new User();
-$user_from_db->fetch(mysqli: $mysqli, id: 1);
+unset($test_user); // remove test_user obj
+$sql = "select count(*) from users;";
+$result = $mysqli->query($sql);
+$result = $result->fetch_array();
+echo 'current # of users: ' . $result['count(*)'] . "\n";
 
+print("-------------- \n");
+print("testing user.pull method on new user obj: \n");
+$user_from_db = new User();
+$user_from_db->pull(mysqli: $mysqli, id: 1);
 print_r($user_from_db);
+
+print("-------------- \n");
+print("testing user.push method: \n");
+$user_from_db->userName = "updated_uname";
+$user_from_db->pull(mysqli: $mysqli, id: 1);
+print_r($user_from_db);
+
+print("-------------- \n");
+print("testing user.delete method: \n");
+$sql = "select count(*) from users;";
+$result = $mysqli->query($sql);
+$result = $result->fetch_array();
+echo 'current # of users: ' . $result['count(*)'] . "\n";
+print("deleting userId:" . $user_from_db->getID() . "\n");
+$user_from_db->delete($mysqli);
+$sql = "select count(*) from users;";
+$result = $mysqli->query($sql);
+$result = $result->fetch_array();
+echo 'current # of users: ' . $result['count(*)'] . "\n";
 
 // if passing around will need to close last
 // dont love

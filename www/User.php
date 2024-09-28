f70dd78b742a4b0000c569d9636ba1874210bf46
justice <81177526+tPeltier@users.noreply.github.com>
2024-09-27 20:20:46 -0400

    update User methods
    i think this is a better approach than before

--- www/User.php
+++ www/User.php
@@ -1,4 +1,7 @@
 <?php
+// TODO: error handling, specifically in regards to the db
+//
+//
 class User implements  DB_functions
 {
 	protected int $id; // or private?
@@ -9,10 +12,20 @@ class User implements  DB_functions
 	protected string $passwd; // or private?
 
 	public function setID(int $id):void { $this->id = $id; } // for testing & creation only rn  TODO: repair
+	public function getID():int { return $this->id; } // for testing & creation only rn  TODO: repair
+	public function setPW(string $pwd):void { $this->passwd = $pwd; } // for testing & creation only rn  TODO: repair
 
-	public function create(mysqli $mysqli):void
+	/**
+	 * Create a db entry for the current user obj
+	 *
+	* TODO: desc
+	*
+	* @param mysqli $mysqli db object
+	* @return void
+	 */
+	public function create(mysqli $mysqli): void
 	{
-		$query = "insert into users(userName, firstName, lastName, email, passwd) values (?,?,?,?,?)";
+		$query = "insert users(userName, firstName, lastName, email, passwd) values (?,?,?,?,?)";
 		$stmt = $mysqli->prepare($query);
 		$userName = $this->userName;
 		$fistName = $this->firstName;
@@ -24,24 +37,47 @@ class User implements  DB_functions
 		$stmt->execute();
 	}
 
+	/**
+	* Delete the user who's user obj this is from db
+	*
+	* TODO: desc
+	*
+	* @param mysqli $mysqli db object
+	* @return void
+	*/
 	public function delete(mysqli $mysqli): void
 	{
 		$query = "delete from users where users.id = (?)";
 		$stmt = $mysqli->prepare($query);
-		$id = this->id;
+		$id = $this->id;
 		$types = "i";
 		$stmt->bind_param($types, $id);
 		$stmt->execute();
 	}
 
-	// NOTE: make static & return obj???
-	public function fetch(mysqli $mysqli, int $id): void
+	/**
+	* Pull a user from db into a user object.
+	*
+	* TODO: desc
+	*
+	* @param mysqli $mysqli db object
+	* @param int $id id of user to pull
+	* @return void
+	*/
+	public function pull(mysqli $mysqli, int $id): void
 	{
-		$query = "select * from users where users.id = $id"; // WARN: best practice???
+		$query = "select * from users where users.id = (?)";
 		$stmt = $mysqli->prepare($query);
+		$id = $id;
+		$types = "i";
+		$stmt->bind_param($types, $id);
 		$stmt->execute();
 		$result = $stmt->get_result();
 		$row = $result->fetch_assoc();
+		if ($row['id'] == null) {
+			print("no user in db with that id");
+			return;
+		}
 		$this->id = $row['id'];
 		$this->userName = $row['userName'];
 		$this->firstName = $row['firstName'];
@@ -50,25 +86,26 @@ class User implements  DB_functions
 		$this->passwd = $row['passwd'];
 	}
 
-	public function insert(mysqli $mysqli, string $field): void
+	/**
+	* Push the current user obj into the db
+	*
+	* TODO: desc
+	*
+	* @param mysqli $mysqli db object
+	* @return void
+	*/
+	public function push(mysqli $mysqli): void
 	{
-		$query = "insert into users(field) values (?) where users.id = (?)";
+		$query = "update users(userName, firstName, lastName, email, passwd) set (?,?,?,?,?) where users.id = (?)";
 		$stmt = $mysqli->prepare($query);
-		$field = $field; // necessary?
 		$id = $this->id;
-		$types = "si";
-		$stmt->bind_param($types, $field, $id);
-		$stmt->execute();
-	}
-
-	public function update(mysqli $mysqli, string $field): void
-	{
-		$query = "update users(field) set (?) where users.id = (?)";
-		$stmt = $mysqli->prepare($query);
-		$field = $field; // necessary?
-		$id = $this->id;
-		$types = "si";
-		$stmt->bind_param($types, $field, $id);
+		$userName = $this->userName;
+		$fistName = $this->firstName;
+		$lastName = $this->lastName;
+		$email = $this->email;
+		$passwd = $this->passwd;
+		$types = "sssssi";
+		$stmt->bind_param($types, $userName, $fistName, $lastName, $email, $passwd, $id);
 		$stmt->execute();
 	}
 }

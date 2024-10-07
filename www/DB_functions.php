2f181b04c771fe143b0668b65bf6c552ff79af35
Nand Patel <112029232+Nandpatel87@users.noreply.github.com>
2024-10-07 18:32:09 -0400

    DB_functions.php

--- www/DB_functions.php
+++ www/DB_functions.php
@@ -2,7 +2,7 @@
 interface  DB_functions
 {
 	// we could make these return bool for p/f
-	public function create(mysqli $mysqli): void;
+	public function create(mysqli $mysqli): int;
 	public function delete(mysqli $mysqli): void;
 	public function pull(mysqli $mysqli, int $id): void;
 	public function push(mysqli $mysqli): void;

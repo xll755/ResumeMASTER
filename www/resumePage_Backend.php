4f5b89fa992c9683b6e056fdebde096022bce7db
Bryce Raper <112665393+mmh387@users.noreply.github.com>
2024-10-30 18:29:01 -0400

    Update resumePage_Backend.php

--- www/resumePage_Backend.php
+++ www/resumePage_Backend.php
@@ -1,8 +1,8 @@
 <?php
 // resumePage_backend.php
-
+//Updated version of resumePage_backend.php that should fix this issue
 session_start();
-$mysqli = require_once"./db_config.php";
+$mysqli = require_once "./db_config.php";
 include "./Validation.php";
 include "./DB_functions.php";
 include "./Resume.php";
@@ -34,37 +34,43 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit();
     }
 
-    $name = strtolower($fileNameCmps[0]);
-    $blob = (string) file_get_contents($fileTmpPath);
-    // echo mb_strlen($blob, '8bit');
-    // var_dump($blob);
+    // Read file contents into a binary blob
+    $blob = file_get_contents($fileTmpPath);
+    if ($blob === false) {
+        echo 'Error reading file!';
+        exit();
+    }
 
+    // Store resume data into the database
     $resume = new Resume();
     $resume->set_userId($user_id);
-    $resume->set_name($name);
+    $resume->set_name(strtolower($fileNameCmps[0]));
     $resume->set_blob($blob);
-    // print($resume->get_blob());
-    // $resume_id = $resume->create($mysqli);
-    $resume_id = 12; // w/o slashes
+    $resume_id = $resume->create($mysqli);
 
-    $r = new Resume();
-    $r->pull($mysqli, $resume_id);
-    // $r->convert_blob2text();
-    // // var_dump($r);
-    // $r->print();
+    if (!$resume_id) {
+        echo 'Error saving resume to database!';
+        exit();
+    }
 
-    // // Directory where uploaded PDFs will be saved
-    // $uploadFileDir = './uploaded_resumes/';
-    // $dest_path = $uploadFileDir . $fileName;
-    //
-    // // Move file to the desired folder
-    // if (!move_uploaded_file($fileTmpPath, $dest_path)) {
-    //     echo 'There was some error moving the file to the upload directory.';
-    //     exit();
-    // }
+    // Retrieve the PDF to confirm it was stored correctly
+    $retrievedResume = new Resume();
+    if ($retrievedResume->pull($mysqli, $resume_id)) {
+        // Check if blob data is retrieved
+        if (is_null($retrievedResume->get_blob())) {
+            echo 'Error retrieving resume from database!';
+            exit();
+        }
 
-    // Redirect user to a page to assist with developing their page
-    // header('Location: user_page_builder.php?file=' . urlencode($fileName));
-    exit();
+        // Optionally convert blob to text for debugging
+        // echo nl2br(htmlspecialchars($retrievedResume->convert_blob2text()));
+
+        // Redirect user to their page builder after successful upload
+        header('Location: user_page_builder.php?resume_id=' . urlencode($resume_id));
+        exit();
+    } else {
+        echo 'Resume retrieval failed. Possible encoding or query error!';
+        exit();
+    }
 }
 ?>

0f163abe359ef6539dd54b35924fa674c342e3f1
justice <81177526+tPeltier@users.noreply.github.com>
2024-11-05 14:57:59 -0500

    (user_prompt) fix: correct form's target file
    - change the file that the file targets to match previous filename
      change
        - process.php -> apiscript_caller.php
    - add `session_start()` & php section to file
    - format the html portion of the file

--- www/user_prompt.php
+++ www/user_prompt.php
@@ -1,30 +1,35 @@
+<?php
+session_start();
+?>
 <html>
+
 <body>
 
-<form action="process.php" method="POST">
+  <form action="apiscript_caller.php" method="POST">
 
-Resume Section: <br>
-<select name="section">
-  <option value="objective">Objective/Summary</option>
-  <option value="work_experience">Work Experience</option>
-  <option value="education">Education</option>
-  <option value="skills">Skills</option>
-</select>
+    Resume Section: <br>
+    <select name="section">
+      <option value="objective">Objective/Summary</option>
+      <option value="work_experience">Work Experience</option>
+      <option value="education">Education</option>
+      <option value="skills">Skills</option>
+    </select>
 
-<br><br>
+    <br><br>
 
-User Input: <br>
-<textarea type="text" name="user_input" rows="10" cols="80"></textarea><br>
+    User Input: <br>
+    <textarea type="text" name="user_input" rows="10" cols="80"></textarea><br>
 
-<br><br>
+    <br><br>
 
-Job Description: <br>
-<textarea type="text" name="job_desc" rows="10" cols="80"></textarea><br>
+    Job Description: <br>
+    <textarea type="text" name="job_desc" rows="10" cols="80"></textarea><br>
 
-<br><br>
+    <br><br>
 
-<input type="submit">
-</form>
+    <input type="submit">
+  </form>
 
 </body>
+
 </html>

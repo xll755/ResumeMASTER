54b2a00040ff8ab7332756099a0b3ce4b74c605c
justice <81177526+tPeltier@users.noreply.github.com>
2024-11-10 20:50:35 -0500

    (pdf_rednerer) testing out rendering pdfs
    as of now, this is all placeholder/testing data to experiment with how
    we would like render the pdf for printing.
    
    TODO: determine how to extract the neccessary fields from the pdf
    contents using the existing methods (and what would need to be built to
    accomidate anything missing) to pass to the renderer.

--- www/pdf_renderer.php
+++ www/pdf_renderer.php
@@ -19,20 +19,35 @@ class pdf_render
 	// NOTE: maybe pass in an arr of section headers to use as args?
 	public function render(Resume $r): string
 	{
-		$get = new pdf_parser($r);
-		$render = '';
+		// $get = new pdf_parser($r);
+		// TODO: figure out how to get resume contents into arrays
+		// can use the extraction methods, but those only pull whole chunks of
+		// text
+		// can also refactor the text2hmtl methods to take args other than arrs
 
-		$title = $get->get_section_before(''); //TODO:
-		$render .= $this->render_h1($title);
-		$name = $get->get_section_between('', ''); //TODO:
-		$render .= $this->render_h2($name);
-		// NOTE:
-		// etc
-		// add sections as needed.
-		// results in a large string of html that can be sent to the exporter
-		// for downloading.
-		// different rendering methods could render different "styles" of resume.
+		$render = "<style> h1, h2, h3, h4 { text-align: center; } </style>";
+		$render .= "<html><body>";
+		$personal_arr = array(
+			"name" => "tester",
+			"addr" => "1337 tester drive",
+			"contact" => "tester@tester.com",
+		);
+		$render .= $this->personal_data(info: $personal_arr);
+		$work_arr = array(
+			"job_title" => "lead tester",
+			"job_dates" => "5/2020-4/2021",
+			"job_exper" => "Lead a team of dedicated product testers",
+		);
+		$render .= $this->work_expreience(exper_num: 1, work_exper: $work_arr);
+		$edu_arr = array(
+			"school" => "testing universtiy",
+			"degree" => "masters of testing",
+		);
+		$render .= $this->edu_expereince(school_exper: $edu_arr);
+		$additional_info = "i am also a professional wrestler & studio musician!";
+		$render .= $this->additional_info($additional_info);
 
+		$render .= "</body></html>";
 		return $render;
 	}
 }

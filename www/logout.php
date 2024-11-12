ec291f61fb8933556e3575d460829a977e77573e
Bryce Raper <112665393+mmh387@users.noreply.github.com>
2024-11-12 12:57:18 -0500

    Update logout.php

--- www/logout.php
+++ www/logout.php
@@ -10,6 +10,12 @@ $_SESSION = [];
 // Destroy the session
 session_destroy();
 
+// Set headers to prevent page caching
+header("Cache-Control: no-cache, must-revalidate"); // HTTP 1.1.
+header("Pragma: no-cache"); // HTTP 1.0.
+header("Expires: 0"); // Proxies.
+
+
 // Redirect to the welcome page
 header("Location: ./welcome.html");
 exit();

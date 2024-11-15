9db836434c70b634ffc29acbff51bdd07afc7128
xll755 <112734151+xll755@users.noreply.github.com>
2024-11-15 02:00:16 -0500

    Update uHome.php
    removed a comment. Changed order of a tab.

--- www/uHome.php
+++ www/uHome.php
@@ -92,9 +92,9 @@ session_start();
             <ul class="bar">  											<!-- Creating a tab layout on the bar --> 
                 <li><a href="uHome.php" class="active">Home</a></li>   <!-- Active tab and "web location" -->		
                 <li><a href="resumePage.php">Resume Upload</a></li>	<!-- # are dummy links, replace w/ actual links --> 
-				<li><a href="resume_example.php">Resume Example</a></li>   <!-- Active tab and "web location" -->
-                <li><a href="logout.php">Logout</a></li>
-				<li><a href="comparisonF.php">Qualification Comparison</a></li>
+		<li><a href="resume_example.php">Resume Example</a></li>
+                <li><a href="comparisonF.php">Qualification Comparison</a></li>
+		<li><a href="logout.php">Logout</a></li>
             </ul>
         </nav>
     </div>

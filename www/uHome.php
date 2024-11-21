c9607283b7c0d531e5efc97a217ef657f1fc998b
Nand Patel <nandpatel87@gmail.com>
2024-11-21 15:10:18 -0500

    Update uHome.php

--- www/uHome.php
+++ www/uHome.php
@@ -1,6 +1,15 @@
 <?php
 session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: index.html");
+    exit();
+}
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 ?>
+
 <!-- ResumeMASTER
 	 Lewis Green
 	 CPSC 4910

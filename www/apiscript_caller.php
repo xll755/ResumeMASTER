40e17e19ef84f062101f82f731a0fa7db13fcbc3
justice <81177526+tPeltier@users.noreply.github.com>
2024-11-21 22:57:38 -0500

    [broken] trying to get the py script to call
    it seems that the script is not running at all?
    im not sure how to debug this, its a wip.
    
    the call looks like that because the genai package is in a virtual env,
    so the virtual python env must be used.

--- www/apiscript_caller.php
+++ www/apiscript_caller.php
@@ -6,8 +6,9 @@ $type = 0;
 
 echo "AI Response: <br><br>";
 
-// arguments currently do not work
-$command = escapeshellcmd("python3 resumeMasterAI.py" . " " . $user_input . " " . $job_desc . " " . $type);
+// call does not work? the genai lib is installed in a virtual env, hence the
+// first part of the call, however it doesnt seem to be running?
+$command = escapeshellcmd("/app/.venv/bin/python3 ./resumeMasterAI.py" . " " . $user_input . " " . $job_desc . " " . $type);
 $output = shell_exec($command);
 
 // Display the output from Python

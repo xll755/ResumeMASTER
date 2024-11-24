05ee6bdd710e7c2aeae2a0aed5ee8c89b007ebe6
justice <81177526+tPeltier@users.noreply.github.com>
2024-11-24 00:28:03 -0500

    (apiscript_caller) correct script call & args
    - fixing the way that the script is called to support the fact that it
    runs in a venv.
    - also altering the way that its args are passed to ensure that each arg
    gets correctly processesed and passed.
    - chaning `type` to be the "section" selected in the form ( hardcoded
    int -> passed string )

--- www/apiscript_caller.php
+++ www/apiscript_caller.php
@@ -2,13 +2,11 @@
 
 $user_input = $_POST["user_input"];
 $job_desc = $_POST["job_desc"];
-$type = 0;
+$type = $_POST["section"];
 
 echo "AI Response: <br><br>";
 
-// call does not work? the genai lib is installed in a virtual env, hence the
-// first part of the call, however it doesnt seem to be running?
-$command = escapeshellcmd("/app/.venv/bin/python3 ./resumeMasterAI.py" . " " . $user_input . " " . $job_desc . " " . $type);
+$command = "/app/.venv/bin/python3 /var/www/html/resumeMasterAI.py " . escapeshellarg($user_input) . " " . escapeshellarg($job_desc) . " " . escapeshellarg($type);
 $output = shell_exec($command);
 
 // Display the output from Python

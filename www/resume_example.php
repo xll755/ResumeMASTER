ca3f2f3e8a2559ea1c349d87ff8ebc524a095502
xll755 <112734151+xll755@users.noreply.github.com>
2024-12-07 15:46:57 -0500

    Update resume_example.php active link
    changed active link from uHome.php to resume_example.php

--- www/resume_example.php
+++ www/resume_example.php
@@ -76,10 +76,10 @@ form for the user.
    <link rel="stylesheet" href="css/styles.css">
 	   <nav>
             <ul class="bar">  <!-- Creating a tab layout on the bar -->
-                <li><a href="uHome.php" class="active">Home</a></li>  <!-- Active tab and "web location" -->
+                <li><a href="uHome.php">Home</a></li>
                 <li><a href="./my_resumes.php">My Resumes</a></li>
                 <li><a href="./FrontEnd_createResume.php">Create Resume</a></li>
-                <li><a href="resume_example.php">Example Resume</a></li>
+                <li><a href="resume_example.php" class="active">Example Resume</a></li> <!-- Active tab and "web location" -->
                 <!-- <li><a href="comparisonF.php">Qualification Comparison</a></li> -->
                 <li><a href="logout.php">Logout</a></li>
             </ul>

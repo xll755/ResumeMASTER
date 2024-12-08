51a34e7b44086b4c3be3021d61d25c69a6903fdc
justice <81177526+tPeltier@users.noreply.github.com>
2024-12-08 17:59:15 -0500

    fix(style) transition to stylesheet + format src

--- www/front-end/my-resumes.php
+++ www/front-end/my-resumes.php
@@ -46,46 +46,46 @@ if (isset($_GET['action']) && isset($_GET['id'])) {
         <link href="../css/styles.css" rel="stylesheet">
     </head>
     <body>
-       <div class="container">
-           <nav>
-                <ul class="bar">  <!-- Creating a tab layout on the bar -->
-                    <li><a href="./home.php">Home</a></li>
-                    <li><a href="./my-resumes.php" class="active">My Resumes</a></li> <!-- Active tab and "web location" -->
-                    <li><a href="./create-resume.php">Create Resume</a></li>
-                    <li><a href="./example-resume.php">Example Resume</a></li>
-                    <li><a href="./edit-user.php">Edit My Information</a></li>
-                    <li><a href="./logout.php">Logout</a></li>
-                </ul>
-            </nav>
+        <nav>
+            <ul class="bar">  <!-- Creating a tab layout on the bar -->
+                <li><a href="./home.php">Home</a></li>
+                <li><a href="./my-resumes.php" class="active">My Resumes</a></li> <!-- Active tab and "web location" -->
+                <li><a href="./create-resume.php">Create Resume</a></li>
+                <li><a href="./example-resume.php">Example Resume</a></li>
+                <li><a href="./edit-user.php">Edit My Information</a></li>
+                <li><a href="./logout.php">Logout</a></li>
+            </ul>
+        </nav>
+        <div class="general-container">
+            <h1>My Resumes</h1>
+            <?php if (sizeof($resumes) > 0) : ?>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>NAME</th>
+                        <th>View</th>
+                        <th>Edit</th>
+                        <th>Download</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($resumes as $r): ?>
+                    <tr>
+                        <td><?php echo $r[0]; ?></td>
+                        <td><?php echo $r[1]; ?></td>
+                        <td><a href="?action=view&id=<?php echo $r[0];?>">View</a></td>
+                        <td><a href="?action=edit&id=<?php echo $r[0];?>">Edit</a></td>
+                        <td><a href="?action=download&id=<?php echo $r[0];?>">Download</a></td>
+                        <td><a href="?action=delete&id=<?php echo $r[0];?>" onclick="return confirm('Are you sure?');">Delete</a></td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+            <?php else : ?>
+            <h3>You currently don't have any resumes created. <a href="./create-resume.php">Create one?</a></h3>
+            <?php endif; ?>
         </div>
-        <h1>My Resumes</h1>
-        <?php if (sizeof($resumes) > 0) : ?>
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>NAME</th>
-                    <th>View</th>
-                    <th>Edit</th>
-                    <th>Download</th>
-                    <th>Delete</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($resumes as $r): ?>
-                <tr>
-                    <td><?php echo $r[0]; ?></td>
-                    <td><?php echo $r[1]; ?></td>
-                    <td><a href="?action=view&id=<?php echo $r[0];?>">View</a></td>
-                    <td><a href="?action=edit&id=<?php echo $r[0];?>">Edit</a></td>
-                    <td><a href="?action=download&id=<?php echo $r[0];?>">Download</a></td>
-                    <td><a href="?action=delete&id=<?php echo $r[0];?>" onclick="return confirm('Are you sure?');">Delete</a></td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-        <?php else : ?>
-        <h3>You currently don't have any resumes created. <a href="./create-resume.php">Create one?</a></h3>
-        <?php endif; ?>
     </body>
 </html>
